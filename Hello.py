795fadf4124bb05362934b4894ccb40e166dccfd
Payal Shahapure <shahapurepayal@gmail.com>
2025-12-10 20:09:20 +0530

    area peri

--- Hello.py
+++ Hello.py
@@ -1,4 +1,6 @@
 import arithmetic
+import geometry
+
 
 print("Hello World!!")
 
@@ -8,3 +10,10 @@ num2=int(input("Enter num2:"))
 arithmetic.add(num1,num2)
 
 arithmetic.sub(num1,num2)
+
+
+len=int(input("Enter len:"))
+br=int(input("Enter br:"))
+
+geometry.calc_rect_area(len,br)
+geometry.calc_rect_peri(len,br)
